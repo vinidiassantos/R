0726049233afd7cba6c6f01b64862a9c01ed455e
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-23 13:17:43 -0300

    Update Training.R

--- Training.R
+++ Training.R
@@ -1,2 +1,13 @@
-library("ggplot2")
-library(palmerpenguins")
+name <- "Vini"
+age <- 38
+
+name  # output "Vini"
+age   # output 40
+
+
+# Assign the same value to multiple variables in one line
+var1 <- var2 <- var3 <- "Orange"
+# Print variable values
+var1
+var2
+var3
