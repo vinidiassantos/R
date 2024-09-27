13841e3492f0dda0d62637826b7ade17312438ec
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-09-27 08:47:04 -0300

    Update codereview.r

--- codereview.r
+++ codereview.r
@@ -4,5 +4,5 @@ age <- c(64, 78, 86, 92, 112)
 names <-(João, Marie, Julie)
 df <- data.frame(row.names = Marie, Julie, João), row.names = NULL, check.rows = FALSE, check.names = TRUE, stringsAsFactors = default.stringsAsFactors())
 else {
-   "More old"
+   "More old and more bigger"
 }
